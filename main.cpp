46097c6b34f1beb6efb04331d47400303b4a7f8e
iгорь <igor.s@standuply.com>
2021-03-11 12:40:39 +0700

    change test cases

--- main.cpp
+++ main.cpp
@@ -1,36 +1,29 @@
-#include <iostream>
 #include <thread>
 #include <vector>
-#include <functional>
-#include <chrono>
 
 #include "tsqueue.h"
 
 using namespace std;
 
 int main() {
-    tsqueue<int> q({1, 2, 3, 4, 5});
+    tsqueue<int> q;
 
     vector<thread> workers;
-    const int numOfWorkers = 5;
-    const int numOfOperations = 100000;
+    const int numOfWorkers = 10;
+    const int numOfOperations = 10000;
 
     for (int i = 0; i < numOfWorkers; i++) {
         workers.push_back(thread([&]() {
             for (int j = 0; j < numOfOperations; j++) {
-                q.push(j);
+                q.push(200);
             }
-
-            q.push(1337);
         }));
     }
 
     for (int i = 0; i < numOfWorkers; i++) {
         workers.push_back(thread([&]() {
-            q.tryPop();
-
             for (int j = 0; j < numOfOperations; j++) {
-                q.tryPopFor(chrono::seconds(2));
+                q.tryPopFor(chrono::seconds(1));
             }
         }));
     }
